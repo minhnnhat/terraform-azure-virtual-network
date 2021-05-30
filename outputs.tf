4ddcb04e82ab6451a9402eb1cc52edd2aa9fcd87
NhatMinh <ngnhatminh49@gmail.com>
2021-05-30 16:40:39 +0700

    Add subnet ids in output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "az_vnic_ids" {
   #value = toset([for vnicid in azurerm_network_interface.az_vnic : vnicid.id])
   value = toset([for vnicid in azurerm_network_interface.az_vnic : vnicid.id])
 }
+
+output "az_subnet_ids" {
+  value = toset([for subnetid in azurerm_subnet.az_subnet : subnetid.id])
+}
